904e83e99b093e1517ab20e1bb9d2088dc08c940
nour fawzy <nourfawzy292001@gmail.com>
2024-06-30 02:58:14 +0300

    create json server and implement get topics function to get all topics from json server

--- src/app/Compontents/TopicsList/TopicsList.tsx
+++ src/app/Compontents/TopicsList/TopicsList.tsx
@@ -2,27 +2,51 @@ import { RiDeleteBinLine } from "react-icons/ri";
 import { MdOutlineModeEdit } from "react-icons/md";
 import Link from "next/link";
 
-export default function TopicList() {
-  const topicId = "123"; // Replace with actual dynamic ID
+const getTopics = async () => {
+  try {
+    const res = await fetch("http://localhost:3001/topics");
+    if (!res.ok) {
+      throw new Error("failed to fetch topics");
+    }
+    return res.json();
+  } catch (error) {
+    console.log("error loading topics", error);
+    return []; // Return an empty array if there's an error
+  }
+};
+
+export default async function TopicList() {
+  const topics = await getTopics(); // Directly assign the fetched topics
 
   return (
     <>
-      <div className="border border-slate-300 my-4 p-4 flex justify-between items-center">
-        <div>
-          <h2 className="font-bold text-2xl">topic title</h2>
-          <h2>topic description</h2>
-        </div>
-        <div className="flex">
-          <Link href={`/EditTopic/${topicId}`}>
-            <MdOutlineModeEdit
-              className="text-yellow-500 me-3 cursor-pointer"
-              size={26}
-            />
-          </Link>
-
-          <RiDeleteBinLine className="text-red-600" size={26} />
-        </div>
-      </div>
+      {topics.length > 0 ? (
+        topics.map((t: any) => (
+          <div
+            key={t.id}
+            className="border border-slate-300 my-4 p-4 flex justify-between items-center"
+          >
+            <div>
+              <h2 className="font-bold text-2xl">{t.title}</h2>
+              <h2>{t.description}</h2>
+            </div>
+            <div className="flex">
+              <Link href={`/EditTopic/${t.id}`}>
+                <MdOutlineModeEdit
+                  className="text-yellow-500 me-3 cursor-pointer"
+                  size={26}
+                />
+              </Link>
+              <RiDeleteBinLine
+                className="text-red-600 cursor-pointer"
+                size={26}
+              />
+            </div>
+          </div>
+        ))
+      ) : (
+        <p>No topics found.</p>
+      )}
     </>
   );
 }
