ee771098176375f3b299b67fefeeef257c9f894b
Nourfawzy-x <122273948+Nourfawzy-x@users.noreply.github.com>
2024-07-03 07:01:08 +0300

    Update mongodb.js

--- src/app/Libs/mongodb.js
+++ src/app/Libs/mongodb.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 const connectMongoDB = async () => {
   try {
-    console.log("MONGO_URI:", process.env.MONGO_URI); // Add this line to debug
+    console.log("MONGO_URI:", process.env.MONGO_URI);
     await mongoose.connect(process.env.MONGO_URI);
     console.log("connected to mongoDB");
   } catch (error) {
